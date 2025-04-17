c104f8284501df3a9eb222cb4d016ce43d5974c2
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-04-17 09:22:48 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,9 +1,5 @@
-import os
 import instaloader
 
-USERNAME = os.environ.get("IG_USERNAME")
-PASSWORD = os.environ.get("IG_PASSWORD")
-
 loader = instaloader.Instaloader()
-loader.login(USERNAME, PASSWORD)
-loader.save_session_to_file()
+loader.login("codexbots", "aditya@2005")
+loader.save_session_to_file("session-codexbots")
